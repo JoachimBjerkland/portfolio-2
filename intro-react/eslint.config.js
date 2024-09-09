a6a01fcff8f20ac5798740e5f19ee656069afd05
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2024-09-09 10:35:56 +0200

    portfolio 2

--- intro-react/eslint.config.js
+++ intro-react/eslint.config.js
@@ -1,32 +1,28 @@
-import baseConfig from "../eslint.config";
-const { resolve } = require("node:path");
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-const project = resolve(process.cwd(), "tsconfig.app.json");
-
-export default {
-  ...baseConfig,
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    ...baseConfig.extends,
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "eslint.config.js"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    ...baseConfig.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-};
+)
--- intro-react/src/App.tsx
+++ intro-react/src/App.tsx
@@ -1,77 +1,12 @@
-import { useState } from "react";
-import Streaks from "./Streaks";
-import Welcome from "./Welcome";
-import Habits from "./Habits";
-import type { Action, Habit as HabitType, Streak as StreakType } from "./types";
-import { ofetch } from "ofetch";
-
-const user = {
-  name: "Alfred",
-  age: 20,
-};
-
 function App() {
-  const [streaks, setStreaks] = useState<StreakType[]>([]);
-  const [habits, setHabits] = useState<HabitType[]>([]);
+  console.log('Console log i App');
 
-  ofetch("http://localhost:3000/habits").then((res) => {
-    console.log("data fetched", res);
-  });
-
-  const add = (habit: HabitType) => {
-    setHabits((prev) => [...prev, habit]);
-    setStreaks((prev) => [
-      ...prev,
-      { id: crypto.randomUUID(), habitId: habit.id, streakCount: 0 },
-    ]);
-  };
-
-  const remove = (id: string) => {
-    setHabits((prev) => prev.filter((habit) => habit.id !== id));
-    setStreaks((prev) => prev.filter((streak) => streak.habitId !== id));
-  };
-
-  const handleHabitMutation = (action: Action, habit: HabitType) => {
-    switch (action) {
-      case "add":
-        add(habit);
-        break;
-      case "remove":
-        remove(habit.id);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const updateStreakCount = (id: string) => {
-    setStreaks((prevStreaks) =>
-      prevStreaks.map((streak) => {
-        if (streak.id === id) {
-          return { ...streak, streakCount: streak.streakCount + 1 };
-        }
-        return streak;
-      })
-    );
-  };
-
-  const calculateTotalStreaks = () => {
-    let total = 0;
-    for (const streak of streaks) {
-      total += streak.streakCount;
-    }
-    return total;
-  };
+  const name = "Alfred";
+  const age = 20;
 
   return (
     <main>
-      <Welcome user={user} />
-      <Streaks
-        streaks={streaks}
-        updateStreakCount={updateStreakCount}
-        total={calculateTotalStreaks()}
-      />
-      <Habits habits={habits} handleHabitMutation={handleHabitMutation} />
+      <h1>Velkommen til Streaky, {name}. Du er {age + 1} år.</h1>
     </main>
   );
 }
